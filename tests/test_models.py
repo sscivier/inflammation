aafaa8674c952a12548451e09edfa9dd7f8a9645
Sam Scivier <samscivier@gmail.com>
2022-10-17 11:04:46 +0100

    parameterize tests for mean function

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,30 +5,16 @@ import numpy as np
 import numpy.testing as npt
 from unittest.mock import patch
 
-def test_daily_mean_zeros():
-    """Test that mean function works for an array of zeros."""
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[1, 2], [3, 4], [5, 6]], [3, 4]),
+    ])
+def test_daily_mean(test, expected):
+    """Test mean function works for array of zeroes and positive integers."""
     from inflammation.models import daily_mean
-
-    # NB: the comment 'yapf: disable' disables automatic formatting using
-    # a tool called 'yapf' which we have used when creating this project
-    test_array = np.array([[0, 0],
-                           [0, 0],
-                           [0, 0]])  # yapf: disable
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(np.array([0, 0]), daily_mean(test_array))
-
-
-def test_daily_mean_integers():
-    """Test that mean function works for an array of positive integers."""
-    from inflammation.models import daily_mean
-
-    test_array = np.array([[1, 2],
-                           [3, 4],
-                           [5, 6]])  # yapf: disable
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(np.array([3, 4]), daily_mean(test_array))
+    npt.assert_array_equal(np.array(expected), daily_mean(np.array(test)))
 
 
 def test_daily_min_string():
